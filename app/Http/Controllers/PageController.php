cd12591b8369c6821c625a97f712853db8af48e6
peppeluc22 <gluca0260@gmail.com>
2023-05-23 15:01:49 +0200

    created page controller to take all movies from db

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,7 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Movie;
+
 class PageController extends Controller
 {
-    //
+
+    public function index()
+    {
+        $movies = Movie::all();
+
+        return view('home', compact('movies'));
+    }
+
+
 }

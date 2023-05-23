8ec9f99bf151487020c6cea1c2edba0aaefbad05
peppeluc22 <gluca0260@gmail.com>
2023-05-23 15:05:38 +0200

    add dynamic page title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,9 @@
 @extends('layout.app')
 
+@section('page_title')
+    Homepage
+@endsection
+
 @section('content')
     @include('partials.main')
 @endsection
--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -1,5 +1,9 @@
 @extends('layout.app')
 
+@section('page_title')
+    {{ $movie->title }}
+@endsection
+
 @section('content')
     @include('partials.movieCard')
 @endsection
